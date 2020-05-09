8f3c43edebfccc88433b39fc9ddf707f4fc77f8d
vroyibg <vroyibg@gmail.com>
2020-05-09 17:10:48 +0700

    show wallet transactions

--- src/WalletPage.js
+++ src/WalletPage.js
@@ -6,15 +6,20 @@ import {getEndpoint} from "./api";
 const WalletPage = () => {
   const {id} = useParams()
   const [wallet, setWallet] = React.useState(null)
+  const [transactions, setTransactions] = React.useState([])
   React.useEffect(() => {
-    get(getEndpoint() + `/wallets/${id}`).then(data => setWallet(data)).catch(e => alert(e))
+    get(`${getEndpoint()}/wallets/${id}`).then(data => setWallet(data)).catch(e => alert(e))
   }, [])
+  React.useEffect(() => {
+    get(getEndpoint() + "/transactions", {walletId: id}).then(data => setTransactions(data.items))
+  }, [wallet])
   if (!wallet) {
     return <h1>Loading</h1>
   }
   return <div>
     <div>{wallet.name}</div>
     <div>{wallet.balance}</div>
+    {transactions.forEach(transaction => <div>{transaction.amount}</div>)}
   </div>
 }
 
