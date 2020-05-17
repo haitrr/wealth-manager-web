823457f52ed36774ca0b34c9c39cd41c8c490f5a
vroyibg <vroyibg@gmail.com>
2020-05-17 14:07:06 +0700

    add br

--- src/HomePage.js
+++ src/HomePage.js
@@ -4,6 +4,7 @@ import {Link} from "react-router-dom";
 const HomePage = () => <div>
   <h2>Home Page</h2>
   <Link to="/wallets">Wallets</Link>
+  <br/>
   <Link to="transactions/add">Add Transaction</Link>
 </div>
 export default HomePage;
