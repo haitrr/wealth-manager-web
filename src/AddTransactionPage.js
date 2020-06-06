f34bc0d31cd254f2158c521c29d375ca5d763446
vroyibg <vroyibg@gmail.com>
2020-06-06 17:02:42 +0700

    use useForm

--- src/AddTransactionPage.js
+++ src/AddTransactionPage.js
@@ -1,63 +1,53 @@
 import {getEndpoint} from "./api";
-import {Formik} from "formik";
 import React from "react";
-import {get, post} from "./httpClient";
+import {post} from "./httpClient";
 import history from "./history";
-import useTransactionsCategories from "./useTransactionCategories";
+import useWallets from "./useWallets";
+import useForm from "./useForm";
+import CategorySelect from "./CategorySelect";
 
 const AddTransactionPage = () => {
-  const [wallets, setWallets] = React.useState([])
-  React.useEffect(() => {
-    get(getEndpoint() + "/wallets").then(d => setWallets(d.items))
-  }, [])
-  const categories = useTransactionsCategories()
+  const wallets = useWallets();
 
-  return <Formik initialValues={{walletId: null}} onSubmit={(values, {setSubmitting}) => {
+  const [values, handleChange, handleSubmit, isSubmitting] = useForm((values, setSubmitting) => {
     values.walletId = parseInt(values.walletId)
     values.categoryId = parseInt(values.categoryId)
     setSubmitting(true)
     post(getEndpoint() + "/transactions", values).then(d => {
       setSubmitting(false)
+      console.log(d)
       history.push("/")
     }).catch(e => {
       alert(e)
       setSubmitting(false)
     })
-  }}>
-    {({handleSubmit, values, isSubmitting, handleBlur, handleChange}) => {
-      return <form onSubmit={handleSubmit}>
-        <div>
-          <label>Amount :</label>
-          <input required name="amount" value={values.amount} onChange={handleChange} type="number"
-                 placeholder="Amount"/>
-        </div>
-        <div>
-          <label>Wallet :</label>
-          <select required name="walletId" onChange={handleChange} onBlur={handleBlur} value={values.walletId}>
-            <option/>
-            {wallets.map((wallet) => {
-              return <option key={wallet.id} value={wallet.id}>{wallet.name}</option>
-            })}
-          </select>
-        </div>
-        <div>
-          <label>Category :</label>
-          <select
-            required name="categoryId"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.categoryId}
-          >
-            <option/>
-            {categories.map(category => {
-              return <option key={category.id} value={category.id}>{category.name}</option>
-            })}
-          </select>
-        </div>
-        <button type="submit" disabled={isSubmitting}>Add</button>
-      </form>
-    }}
-  </Formik>
+  })
+
+  return <form onSubmit={handleSubmit}>
+    <div>
+      <label>Amount :</label>
+      <input required name="amount" value={values.amount} onChange={handleChange} type="number"
+             placeholder="Amount"/>
+    </div>
+    <div>
+      <label>Wallet :</label>
+      <select required name="walletId" onChange={handleChange} value={values.walletId}>
+        <option/>
+        {wallets.map((wallet) => {
+          return <option key={wallet.id} value={wallet.id}>{wallet.name}</option>
+        })}
+      </select>
+    </div>
+    <div>
+      <label>Category :</label>
+      <CategorySelect
+        required name="categoryId"
+        onChange={handleChange}
+        value={values.categoryId}
+      />
+    </div>
+    <button type="submit" disabled={isSubmitting}>Add</button>
+  </form>
 }
 
 export default AddTransactionPage;
