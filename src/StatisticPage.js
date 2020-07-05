6439fc11be03177f5e5f3ba239d5d6eba5626b17
vroyibg <vroyibg@gmail.com>
2020-07-05 17:27:09 +0700

    refactor code

--- src/StatisticPage.js
+++ src/StatisticPage.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ExpenseStatistic from "./ExpenseStatistic";
-import moment from 'moment'
 
 const StatisticPage = () => {
   return <div>
     <h1>Statistic</h1>
-    <ExpenseStatistic ></ExpenseStatistic>
+    <ExpenseStatistic />
   </div>
 }
 
