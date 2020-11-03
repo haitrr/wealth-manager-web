b7782e8c0351ecc6a4478f2b32c02fa7b047d8cb
vroyibg <vroyibg@gmail.com>
2020-11-03 09:13:30 +0700

    transaction category tree ts

---
+++ src/useTransactionCategoriesTree.ts
@@ -0,0 +1,44 @@
+import React from "react";
+import {get} from "./httpClient";
+import {getEndpoint} from "./api";
+
+const buildCategoriesTree = (categories: any): any => {
+  const root: CategoryTree = {
+    name: "All",
+    id: -1,
+    children: []
+  }
+  categories.forEach((c: any) => {
+    if (!c.children) {
+      c.children = []
+    }
+    if (c.parentId) {
+      const parent = categories.find((t: any) => t.id === c.parentId)
+      if (parent.children) {
+        parent.children.push(c)
+      }
+    } else {
+      root.children.push(c)
+    }
+  })
+  return root;
+}
+
+export interface CategoryTree {
+  name: string;
+  id: number;
+  children: CategoryTree[];
+}
+
+const useTransactionCategoriesTree = (): [CategoryTree | null] => {
+  const [tree, setTree] = React.useState<CategoryTree | null>(null)
+  React.useEffect(() => {
+    get(`${getEndpoint()}/transaction-categories`).then(data => {
+      const tree = buildCategoriesTree(data.items)
+      setTree(tree);
+    })
+  }, [])
+  return [tree]
+}
+
+export default useTransactionCategoriesTree;
