2283631d791a4012c33c4bb6f87ae5b48ab12911
vroyibg <vroyibg@gmail.com>
2020-11-29 17:23:16 +0700

    add padding

--- src/AddTransactionPage.js
+++ src/AddTransactionPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import AddTransactionForm from "./AddTransactionForm";
 
 const AddTransactionPage = () => {
-  return <div>
+  return <div style={{padding: "1rem"}}>
     <h1>Add transaction</h1>
     <AddTransactionForm/>
   </div>
