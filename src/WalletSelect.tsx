f32a0481250e99e3f38ad8d9e75edffcbc62f9cb
vroyibg <vroyibg@gmail.com>
2020-11-29 17:43:36 +0700

    fix wallet default value

--- src/WalletSelect.tsx
+++ src/WalletSelect.tsx
@@ -10,6 +10,12 @@ interface Props {
 
 const WalletSelect: React.FC<Props> = ({onChange, value, required}) => {
   const wallets = useWallets();
+
+  // default to be first value
+  if (wallets[0] && !value && onChange) {
+    onChange(wallets[0].id)
+  }
+
   return <Select defaultActiveFirstOption onChange={onChange} value={value} aria-required={required}>
     {wallets.map((wallet) => {
       return <Select.Option key={wallet.id} value={wallet.id}>{wallet.name}</Select.Option>
