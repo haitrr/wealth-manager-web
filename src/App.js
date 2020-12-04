20d22a8e6c26ecaa914baea8662729a08b6a454d
vroyibg <vroyibg@gmail.com>
2020-12-05 06:46:42 +0700

    integrate redux

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ import LoginPage from "./LoginPage";
 import {GetToken} from "./Jwt";
 import history from "./history";
 import 'antd/dist/antd.css';
+import RootReducer from "./reducer";
+import {applyMiddleware, createStore} from "redux";
+import thunk from 'redux-thunk';
+import {Provider} from "react-redux";
 
 export const AuthenticationContext = React.createContext([{}, null])
+const store = createStore(RootReducer, applyMiddleware(thunk))
 
 function App() {
   const [authentication, setAuthentication] = React.useState({})
@@ -17,18 +22,20 @@ function App() {
     }
   }
   return (
-    <AuthenticationContext.Provider value={[authentication, setAuthentication]}>
-      <Router history={history}>
-        <Switch>
-          <Route path="/login">
-            <LoginPage/>
-          </Route>
-          <Route>
-            <AuthenticatedPage/>
-          </Route>
-        </Switch>
-      </Router>
-    </AuthenticationContext.Provider>
+    <Provider store={store}>
+      <AuthenticationContext.Provider value={[authentication, setAuthentication]}>
+        <Router history={history}>
+          <Switch>
+            <Route path="/login">
+              <LoginPage/>
+            </Route>
+            <Route>
+              <AuthenticatedPage/>
+            </Route>
+          </Switch>
+        </Router>
+      </AuthenticationContext.Provider>
+    </Provider>
   );
 }
 
